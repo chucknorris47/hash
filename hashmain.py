f08f9b9c5c436d691f7c8525d91ce00aa2ce6e9a
CuckNorris47 <philipp.hildenbrand1998@live.de>
2018-06-27 18:04:05 +0200

    Datei kann eingelesen werden

---
+++ hashmain.py
@@ -0,0 +1,11 @@
+
+
+def main():
+	targetString = "af6e56aacd8097c3fb7579b08ecb33d87a040f6ab58067a4b8ff9f5b747abd9be"
+
+	with open("German_de_DE.dic", "r") as fh:
+		content = fh.readlines()
+
+
+if __name__ == "__main__":
+	main()
