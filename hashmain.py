dc8edc1bb926e8261bc155186e554071d32deb25
CuckNorris47 <philipp.hildenbrand1998@live.de>
2018-07-01 16:02:06 +0200

    fertiger Code

--- hashmain.py
+++ hashmain.py
@@ -1,10 +1,20 @@
-
+from hashlib import sha256
+from time import sleep
 
 def main():
-	targetString = "af6e56aacd8097c3fb7579b08ecb33d87a040f6ab58067a4b8ff9f5b747abd9be"
-
+	targetString = "af6e56aacd8097c3fb7597b08ecb3d87a040f6ab58067a4b8ff9f5b747adb9be"
+	
 	with open("German_de_DE.dic", "r") as fh:
 		content = fh.readlines()
+		
+	
+	for element in content:
+					
+		if sha256(element.strip().split("/")[0]).hexdigest() == targetString:
+			print "Uebereinstimmung gefunden bei dem Wort"
+			print element 
+			
+		
 
 
 if __name__ == "__main__":
